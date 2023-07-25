980f1326b09d981fc994be9b24a51590776c92cd
Hasyim-Kai <muhammad.hasyim.c.a@gmail.com>
2023-07-25 17:38:36 +0700

    add user list page

--- src/presentation/pages/user/list.tsx
+++ src/presentation/pages/user/list.tsx
@@ -1,10 +1,28 @@
-import { Link } from 'react-router-dom'
 import DefaultLayout from '../../layout/default'
+import { useQuery } from 'react-query'
+import { useState } from 'react'
+import { fetchUsers } from '../../../infrastructure/user'
+import UserListItem from '../../components/features/user/UserListItem'
+import { User } from '../../../infrastructure/model/user'
+import scrollToTop from '../../../util/ScrollToTop'
 
 
 export default function UserList() {
+  const [page, setPage] = useState<number>(1)
+  function nextPage() { setPage(page + 1); scrollToTop() }
+  function previousPage() { setPage(page - 1); scrollToTop() }
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["user-list", page],
+    queryFn: () => fetchUsers(page),
+    keepPreviousData: true,
+  })
+
   return <DefaultLayout>
-  <div>List</div>  
-    <Link to={`1`}>Detail</Link>
-</DefaultLayout>
+    {isLoading ? <div>Loading</div>
+      : error ? <div>Error</div>
+        : data.data.map((user: User, index: number) => <UserListItem key={index} user={user} />)}
+
+    <button onClick={previousPage}>back</button>
+    <button onClick={nextPage}>next</button>
+  </DefaultLayout>
 }
