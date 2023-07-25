8a6075fbe58732637b55a6e46282d415f80d3600
Hasyim-Kai <muhammad.hasyim.c.a@gmail.com>
2023-07-25 17:38:45 +0700

    add user Detail page

--- src/presentation/pages/user/detail.tsx
+++ src/presentation/pages/user/detail.tsx
@@ -1,12 +1,25 @@
 import { Link, useParams } from 'react-router-dom'
 import DefaultLayout from '../../layout/default'
+import { useQuery } from 'react-query';
+import { fetchUsersDetail } from '../../../infrastructure/user';
 
 
 export default function UserDetail() {
-  const {id} = useParams();
-  
+  const { id } = useParams();
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["user-list", id],
+    queryFn: () => fetchUsersDetail(id),
+  })
+
   return <DefaultLayout>
-    <div>detail {id}</div>
     <Link to={-1}>Go back</Link>
+    {isLoading ? <div>Loading</div>
+      : error ? <div>Error</div>
+        : <section>
+          <img src={data.data.avatar} alt="Profile Photo" />
+          <h1>{data.data.first_name}</h1>
+          <p>{data.data.last_name}</p>
+          <p>{data.data.email}</p>
+        </section>}
   </DefaultLayout>
 }
