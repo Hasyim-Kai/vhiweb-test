5c7376ce2f609c241851d1161cb9ce77a45b5b2c
Hasyim-Kai <muhammad.hasyim.c.a@gmail.com>
2023-07-25 20:02:33 +0700

    add user login function

--- src/infrastructure/user.ts
+++ src/infrastructure/user.ts
@@ -1,3 +1,7 @@
+export interface loginInput {
+    email: string, password: string
+}
+
 export const fetchUsers = async (page: number = 1) => {
     const res = await fetch(`https://reqres.in/api/users?page=${page}`)
     return await res.json()
@@ -5,5 +9,18 @@ export const fetchUsers = async (page: number = 1) => {
 
 export const fetchUsersDetail = async (id: string | undefined) => {
     const res = await fetch(`https://reqres.in/api/users/${id}`)
+    return await res.json()
+}
+
+export const fetchLogin = async (input: loginInput) => {
+    const res = await fetch(`https://reqres.in/api/login`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(input),
+        cache: 'default'
+    })
     return await res.json()
 }
