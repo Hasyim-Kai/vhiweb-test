41edf832a05aef677ded326df26809948c36573c
Hasyim-Kai <muhammad.hasyim.c.a@gmail.com>
2023-07-25 20:02:46 +0700

    add authorization in layout

--- src/presentation/layout/default.tsx
+++ src/presentation/layout/default.tsx
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 interface Props {
   children: JSX.Element | JSX.Element[] | null
 }
 
 export default function DefaultLayout({ children }: Props) {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) { navigate(`/`) }
+  }, [])
+
   return <main className={``}>
     {children}
   </main>
