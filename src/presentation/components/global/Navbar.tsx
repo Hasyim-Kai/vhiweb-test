df12ee385b20cf7947172c10e88a49b726dda037
Hasyim-Kai <muhammad.hasyim.c.a@gmail.com>
2023-07-25 22:04:41 +0700

    add Navbar + styles

--- src/presentation/components/global/Navbar.tsx
+++ src/presentation/components/global/Navbar.tsx
@@ -1,18 +1,23 @@
 import { useState } from "react";
+import s from "./global-features.module.scss";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
-    isBack?: boolean
+    isBack: boolean
 }
 
-function logout() { localStorage.removeItem("token"); navigate(`/`) }
-
-export default function Navbar({ isBack = false }: Props) {
+export default function Navbar({ isBack }: Props) {
     const navigate = useNavigate();
+    function back() { navigate(-1) }
+    function logout() { localStorage.removeItem("token"); navigate(`/`) }
     const [isNavOpen, setIsNavOpen] = useState<boolean>(false)
-    const backBtn = <></>
 
-    return <nav className="">
-        <h1>logo</h1>
-        <button onClick={logout}>Logout</button>
+    return <nav className={s.nav_container}>
+        {isBack ? <div>
+            <button type="button" onClick={back}><img src="/icons/arrow-left.svg" alt="back" /></button>
+            <h3>logo</h3>
+        </div>
+            : <h3>logo</h3>}
+        <button onClick={logout} className={s.logout}>Logout</button>
     </nav>
 }
