3dc61a210d6439a028aba9f3a6524b84701bca61
Hasyim-Kai <muhammad.hasyim.c.a@gmail.com>
2023-07-25 22:05:36 +0700

    Refactor Default layout + styling

--- src/presentation/layout/default.tsx
+++ src/presentation/layout/default.tsx
@@ -1,18 +1,23 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import s from "./layout.module.scss";
+import Navbar from "../components/global/Navbar";
+
 
 interface Props {
   children: JSX.Element | JSX.Element[] | null
+  isBack?: boolean
 }
 
-export default function DefaultLayout({ children }: Props) {
+export default function DefaultLayout({ children, isBack = false }: Props) {
   const navigate = useNavigate();
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (!token) { navigate(`/`) }
   }, [])
 
-  return <main className={``}>
-    {children}
+  return <main>
+    <Navbar isBack={isBack}/>
+    <div className={s.default}>{children}</div>
   </main>
 }
