4fa3e2aa09706a99d1396a09b4e5bb93b552c899
Hasyim-Kai <muhammad.hasyim.c.a@gmail.com>
2023-07-26 13:29:32 +0700

    add img place holder

--- src/presentation/components/features/user/UserListItem.tsx
+++ src/presentation/components/features/user/UserListItem.tsx
@@ -1,15 +1,23 @@
 import { Link } from 'react-router-dom'
 import { User } from '../../../../infrastructure/model/user'
 import s from "./user.module.scss";
+import { useState } from 'react';
 
 interface Props {
     user: User
 }
 
 export default function UserListItem({ user }: Props) {
+    const [imgLoaded, setImgLoaded] = useState<boolean>(false)
+    function handleImgLoaded() { setImgLoaded(true) }
+
     return <Link to={`${user.id}`}><div className={s.user_item_container}>
         <div className={s.profile_photo}>
-            <img src={user.avatar} alt="Profile Photo" />
+            {!imgLoaded && <img src="/img/img-placeholder.png" alt="Placeholder" />}
+            <img src={user.avatar} alt="Profile Photo"
+                onLoad={handleImgLoaded}
+                style={!imgLoaded ? { visibility: 'hidden' } : {}}
+            />
         </div>
         <div className="biodata">
             <h2>{user.first_name}</h2>
