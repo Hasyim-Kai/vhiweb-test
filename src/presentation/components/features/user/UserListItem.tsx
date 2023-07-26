d14e01f6582bcb34e33a78d895d6f33f4a51ddbc
Hasyim-Kai <muhammad.hasyim.c.a@gmail.com>
2023-07-26 11:33:29 +0700

    style user item list

--- src/presentation/components/features/user/UserListItem.tsx
+++ src/presentation/components/features/user/UserListItem.tsx
@@ -1,15 +1,20 @@
 import { Link } from 'react-router-dom'
 import { User } from '../../../../infrastructure/model/user'
+import s from "./user.module.scss";
 
 interface Props {
     user: User
 }
 
 export default function UserListItem({ user }: Props) {
-    return <Link to={`${user.id}`}><div>
-        <img src={user.avatar} alt="Profile Photo" />
-        <h1>{user.first_name}</h1>
-        <p>{user.last_name}</p>
-        <p>{user.email}</p>
+    return <Link to={`${user.id}`}><div className={s.user_item_container}>
+        <div className={s.profile_photo}>
+            <img src={user.avatar} alt="Profile Photo" />
+        </div>
+        <div className="biodata">
+            <h2>{user.first_name}</h2>
+            <p>{user.last_name}</p>
+            <p>{user.email}</p>
+        </div>
     </div></Link>
 }
