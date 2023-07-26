d62b6783ded253ba7f574fa21077a20c8ee9f7a8
Hasyim-Kai <muhammad.hasyim.c.a@gmail.com>
2023-07-26 12:48:39 +0700

    debugging route

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import NoPage from './presentation/pages/user/404'
 import UserDetail from './presentation/components/features/user/UserDetail'
 import UserList from './presentation/components/features/user/UserList'
-import LoginPage from './presentation/pages/a'
+import LoginPage from './presentation/pages/Login'
 
 export default function AppRoutes() {
   return <BrowserRouter>
