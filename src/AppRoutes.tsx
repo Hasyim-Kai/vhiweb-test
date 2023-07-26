28df71d070d8ff79c8e985bb9c93e178170d2451
Hasyim-Kai <muhammad.hasyim.c.a@gmail.com>
2023-07-26 12:42:31 +0700

    debugging route

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -2,12 +2,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import NoPage from './presentation/pages/user/404'
 import UserDetail from './presentation/components/features/user/UserDetail'
 import UserList from './presentation/components/features/user/UserList'
-import Login from './presentation/pages/Login'
+import LoginPage from './presentation/pages/Login'
 
 export default function AppRoutes() {
   return <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<LoginPage />} />
       <Route path="users" element={<UserList />} />
       <Route path="users/:id" element={<UserDetail />} />
       <Route path="*" element={<NoPage />} />
